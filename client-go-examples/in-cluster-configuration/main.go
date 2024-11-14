1ba988e6b4e0cc3326ae5ab75be8f61ebe7587bf
aloys <aloys.zy@hotmail.com>
2024-11-14 16:42:54 +0800

    add in-cluster-configuration

--- client-go-examples/in-cluster-configuration/main.go
+++ client-go-examples/in-cluster-configuration/main.go
@@ -11,14 +11,17 @@ import (
 )
 
 func main() {
+	// 获取集群内配置
 	config, err := rest.InClusterConfig()
 	if err != nil {
 		log.Fatal(err)
 	}
+	// 根据配置信息创建client
 	clientSet, err := kubernetes.NewForConfig(config)
 	if err != nil {
 		log.Fatal(err)
 	}
+	// 循环读取并打印Pod列表
 	for {
 		pods, err := clientSet.CoreV1().Pods("default").List(context.TODO(), metav1.ListOptions{})
 		if err != nil {
@@ -28,6 +31,7 @@ func main() {
 		for i, pod := range pods.Items {
 			log.Printf("%d -> %s/%s\n", i+1, pod.Namespace, pod.Name)
 		}
+		// 定时5秒钟
 		<-time.Tick(5 * time.Second)
 	}
 }
